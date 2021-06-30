d167e0744ff660893163f5c5ef003aac671da263
Rayhan <ridoykhan.cse.web@gmail.com>
2021-07-01 00:44:54 +0600

    Berfey shopping header area add here

---
+++ assets/js/custom.js
@@ -0,0 +1,41 @@
+const header = document.querySelector('header');
+const navMenu = document.querySelector('.nav');
+const closeMenu = document.querySelector('.closeMenu');
+const mobileMenu = document.querySelector('.mobileMenu');
+
+mobileMenu.onclick = function(){
+    navMenu.classList.toggle('active');
+    mobileMenu.classList.toggle('active');
+}
+closeMenu.onclick = function(){
+    navMenu.classList.remove('active');
+    mobileMenu.classList.remove('active');
+}
+
+window.onscroll = function (){
+    this.scrollY > 20 ?
+    header.classList.add('active'):
+    header.classList.remove('active');
+};
+
+$(document).ready(function(){
+
+    // preloader 
+    $(window).on("load", function(){
+        $(".preloader").fadeOut(1000);
+    });
+    
+    // topUp-btn
+    $(window).scroll(function(){
+        if($(this).scrollTop() > 100){
+            $(".topUp-btn").fadeIn(800);
+        } else{
+            $(".topUp-btn").fadeOut(800);
+        }
+    });
+    $(".topUp-btn").click(function(){
+        $(" html,body").animate({scrollTop : 0}, 1000);
+    });
+
+});
+
